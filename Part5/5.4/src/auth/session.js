dff7fcdd52a1fe69abdab4c5d0a7679c1a1b805e
RyanDC1 <ryan.l.dacosta@gmail.com>
2023-10-07 13:52:25 +0530

    update

--- Part5/5.4/src/auth/session.js
+++ Part5/5.4/src/auth/session.js
@@ -1,6 +1,12 @@
 import BlogService from "../services/BlogService";
 import jwt_decode from "jwt-decode";
 
+/**
+ * Note: LocalStorage is used for demo purpose only
+ * actual implementation should be with dedicated authorize call to sever
+ * which will be handled in later parts
+ */
+
 export function initUserSession () {
     const token = localStorage.getItem('token')
     if(token) {
